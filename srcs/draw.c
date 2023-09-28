fd5e8d0ceb2abcf1218eb2e49b0f1e6242f2b0bc
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2023-09-28 11:37:12 +0900

    add map

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,13 +6,74 @@
 /*   By: hikaru <hikaru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 18:17:00 by hikaru            #+#    #+#             */
-/*   Updated: 2023/09/25 12:34:05 by hikaru           ###   ########.fr       */
+/*   Updated: 2023/09/28 11:33:50 by hikaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	reset_display(t_data *data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (y < DISPLAY_Y / 2)
+	{
+		x = 0;
+		while (x < DISPLAY_X)
+		{
+			data->display[y][x] = data->ceiling_rgb;
+			x++;
+		}
+		y++;
+	}
+	while (y < DISPLAY_Y)
+	{
+		x = 0;
+		while (x < DISPLAY_X)
+		{
+			data->display[y][x] = data->floor_rgb;
+			x++;
+		}
+		y++;
+	}
+}
+
+// result_data.height_ratio = atan(0.5 / result_data.distance) / M_PI * 180 / (double)VIEW_ANGLE_Y * (double) 2;
+// result_data.height	= (int)DISPLAY_HALF * result_data.height_ratio;
+// result_data.height_per_pixel = (float)result_data.height / (float)(data->xpm_height / 2);
+
+void	update_display(t_data *data, int index, t_status status)
+{
+	int		j;
+	t_coor	cor;
+	int		pos;
+	char	*mlx_data_addr;
+	int		current_pixel;
+
+	cor.x = (int)(status.pos * data->xpm_width);
+	cor.y = 0;
+	current_pixel = 0;
+	j = DISPLAY_HALF - status.height;
+	while (j < DISPLAY_HALF + status.height)
+	{
+		pos = (cor.y * data->line_length + cor.x * (data->bits_per_pixel / 8));
+		mlx_data_addr = mlx_get_data_addr(data->direction_img[status.dir],
+				&data->bits_per_pixel, &data->line_length, &data->endian);
+		if (0 <= j && j < DISPLAY_Y)
+			data->display[j][index] = *(unsigned int *)(mlx_data_addr + pos);
+		j++;
+		while (((j - DISPLAY_HALF + status.height) / status.height_per_pixel) > current_pixel + 1 && cor.y < data->xpm_height)
+		{
+			current_pixel++;
+			cor.y++;
+		}
+	}
+}
+
+static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
--- srcs/next_frame.c
+++ srcs/next_frame.c
@@ -6,13 +6,13 @@
 /*   By: hikaru <hikaru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 18:01:19 by hikaru            #+#    #+#             */
-/*   Updated: 2023/09/27 21:57:04 by hikaru           ###   ########.fr       */
+/*   Updated: 2023/09/28 11:34:19 by hikaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void	go_straight(t_data *data)
+static void	go_straight(t_data *data)
 {
 	double	step_x;
 	double	step_y;
@@ -28,7 +28,7 @@ void	go_straight(t_data *data)
 	}
 }
 
-void	go_back(t_data *data)
+static void	go_back(t_data *data)
 {
 	double	step_x;
 	double	step_y;
@@ -46,7 +46,7 @@ void	go_back(t_data *data)
 	}
 }
 
-void	go_left(t_data *data)
+static void	go_left(t_data *data)
 {
 	double	step_x;
 	double	step_y;
@@ -64,7 +64,7 @@ void	go_left(t_data *data)
 	}
 }
 
-void	go_right(t_data *data)
+static void	go_right(t_data *data)
 {
 	double	step_x;
 	double	step_y;
@@ -79,69 +79,6 @@ void	go_right(t_data *data)
 		step_y = STEP * sin((data->direction + 90) * M_PI / 180);
 		data->pos_x += step_x;
 		data->pos_y += step_y;
-	}
-}
-
-void	reset_display(t_data *data)
-{
-	int	x;
-	int	y;
-
-	x = 0;
-	y = 0;
-	while (y < DISPLAY_Y / 2)
-	{
-		x = 0;
-		while (x < DISPLAY_X)
-		{
-			data->display[y][x] = data->ceiling_rgb;
-			x++;
-		}
-		y++;
-	}
-	while (y < DISPLAY_Y)
-	{
-		x = 0;
-		while (x < DISPLAY_X)
-		{
-			data->display[y][x] = data->floor_rgb;
-			x++;
-		}
-		y++;
-	}
-}
-
-// result_data.height_ratio = atan(0.5 / result_data.distance) / M_PI * 180 / (double)VIEW_ANGLE_Y * (double) 2;
-// result_data.height	= (int)DISPLAY_HALF * result_data.height_ratio;
-// result_data.height_per_pixel = (float)result_data.height / (float)(data->xpm_height / 2);
-
-void	update_display(t_data *data, int index, t_status status)
-{
-	int		j;
-	t_coor	cor;
-	int		pos;
-	char	*mlx_data_addr;
-	int		current_pixel;
-
-	cor.x = (int)(status.pos * data->xpm_width);
-	cor.y = 0;
-	current_pixel = 0;
-	j = DISPLAY_HALF - status.height;
-	while (j < DISPLAY_HALF + status.height)
-	{
-		pos = (cor.y * data->line_length + cor.x * (data->bits_per_pixel / 8));
-		mlx_data_addr = mlx_get_data_addr(data->direction_img[status.dir],
-				&data->bits_per_pixel, &data->line_length, &data->endian);
-		if (0 <= j && j < DISPLAY_Y)
-			data->display[j][index] = *(unsigned int *)(mlx_data_addr + pos);
-		j++;
-		while (((j - DISPLAY_HALF + status.height) / status.height_per_pixel) > current_pixel + 1)
-		{
-			current_pixel++;
-			cor.y++;
-		}
-		if (cor.y >= data->xpm_height)
-			cor.y = data->xpm_height - 1;
 	}
 }
 
@@ -162,19 +99,9 @@ int	next_frame(int keycode, t_data *data)
 	if (keycode == KEY_D)
 		go_right(data);
 	if (keycode == LEFT_ARROW)
-	{
-		if (data->direction - ANGLE < 0)
-			data->direction = 360 + data->direction - ANGLE;
-		else
-			data->direction -= ANGLE;
-	}
+		turn_left(data);
 	if (keycode == RIGHT_ARROW)
-	{
-		if (data->direction + ANGLE > 360)
-			data->direction = 360 - data->direction + ANGLE;
-		else
-			data->direction += ANGLE;
-	}
+		turn_right(data);
 	reset_display(data);
 	culc_distance(data);
 	return (0);
